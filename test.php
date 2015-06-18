869e125bdc2ee63141a6ac05b4b797c290669e1f
hatemtaleb <hatman_1990@hotmail.fr>
2015-06-18 11:30:54 +0200

    ezrzrze

---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Bonjour le monde</p>'; ?>
+<?php echo '<p>Bonjour le monde</p>'; ?>
+<?php echo '<p>Bonjour le monde</p>'; ?>
+<?php echo '<p>Bonjour le monde</p>'; ?>
+ </body>
+</html>
---
+++ test2.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Bonjour a tous</p>'; ?>
+ <?php echo '<p>Bonjour a tous</p>'; ?>
+ <?php echo '<p>Bonjour a tous</p>'; ?>
+ <?php echo '<p>Bonjour a tous</p>'; ?>
+ <?php echo '<p>Bonjour a tous</p>'; ?>
+ </body>
+</html>
